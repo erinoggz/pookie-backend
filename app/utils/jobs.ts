7267868e2509c83e905f1636ce08cd44d5388f0a
erin deji <erin.deji@gmail.com>
2023-06-20 14:42:10 +0100

    add ratings and review route

--- app/utils/jobs.ts
+++ app/utils/jobs.ts
@@ -2,9 +2,11 @@ import cron from 'node-cron';
 import { container } from 'tsyringe';
 import { BookingService } from '../service/booking.service';
 import { LoggerService } from '../service/logger.service';
+import { RatingsService } from '../service/ratings.service';
 
 const logger: LoggerService = container.resolve(LoggerService);
 const bookingService: BookingService = container.resolve(BookingService);
+const ratingsService: RatingsService = container.resolve(RatingsService);
 
 cron.schedule('*/20 * * * * *', () => {
   logger.log('Running job to validate active booking every 20 seconds');
@@ -22,4 +24,10 @@ cron.schedule('*/20 * * * * *', () => {
   logger.log('Running job to validate completed booking every 20 seconds');
   bookingService.validateCompletedBooking(1);
   logger.log('Completed running job to validate completed bookings');
+});
+
+cron.schedule('* * * * * *', () => {
+  logger.log('Running job to update merchant ratings every 1 min');
+  ratingsService.updateMerchantRatings(1);
+  logger.log('Completed running job to update merchant ratings');
 });
