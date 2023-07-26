0fe8a1af66f98b9e9680dca1bbf3c69666887423
erin deji <erin.deji@gmail.com>
2023-07-26 10:13:22 +0100

    create, credit,  get history, get balnce of wallet

---
+++ app/route/v1/wallet.ts
@@ -0,0 +1,15 @@
+import express, { Router } from 'express';
+import { container } from 'tsyringe';
+import authMiddleware from '../../middleware/auth.middleware';
+import WalletController from '../../controller/wallet.controller';
+
+const WalletRouter: Router = express.Router();
+const walletController: WalletController = container.resolve(WalletController);
+
+WalletRouter.get('/balance', authMiddleware, walletController.walletBalance).get(
+  '/history',
+  authMiddleware,
+  walletController.walletHistory
+);
+
+export default WalletRouter;
