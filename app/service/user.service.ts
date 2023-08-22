fd4d8f7b8be8d8f1882af660f13e7f965d68a22a
erin deji <erin.deji@gmail.com>
2023-08-22 14:16:27 +0100

    revert

--- app/service/user.service.ts
+++ app/service/user.service.ts
@@ -7,7 +7,9 @@ import User, { IUserModel } from '../model/user.model';
 import PaginationService from './pagination.service';
 import StatusCodes from '../lib/response/status-codes';
 import { VerficationService } from './verification.service';
-
+import { EventVerifier } from '@complycube/api';
+import config from '../config/config';
+const eventVerifier = new EventVerifier(config.complycube.complycube_webhook_secret);
 @injectable()
 export class UserService {
   pagination: PaginationService<IUserModel>;
@@ -93,6 +95,57 @@ export class UserService {
       return Helpers.CustomException(
         StatusCodes.UNPROCESSABLE_ENTITY,
         error?.message
+      );
+    }
+  };
+
+  public complycubeWebhook = async (
+    req: any
+  ): Promise<ISuccess | ErrnoException> => {
+    let event;
+    try {
+      const signature = req.headers['complycube-signature'];
+      event = eventVerifier.constructEvent(JSON.stringify(req.body), signature);
+
+      // Handle the event
+      switch (event.type) {
+        case 'check.completed': {
+          const checkId = event.payload.id;
+          const checkOutCome = event.payload.outcome;
+          console.log(`Check ${checkId} completed with outcome ${checkOutCome}`);
+          console.log('payload', event.payload);
+          break;
+        }
+        case 'check.pending': {
+          const checkId = event.payload.id;
+          console.log(`Check ${checkId} is pending`);
+          break;
+        }
+        case 'check.failed': {
+          const checkId = event.payload.id;
+          console.log(`Check ${checkId} is failed`);
+          break;
+        }
+        case 'check.completed.clear': {
+          const checkId = event.payload.id;
+          console.log(`Check ${event.payload} is check.completed.clear`);
+          break;
+        }
+        case 'check.completed.rejected': {
+          const checkId = event.payload.id;
+          console.log(`Check ${event.payload} is check.completed.rejected`);
+          break;
+        }
+        // ... handle other event types
+        default: {
+          // Unexpected event type
+          return Helpers.CustomException(StatusCodes.BAD_REQUEST, null);
+        }
+      }
+    } catch (error) {
+      return Helpers.CustomException(
+        StatusCodes.UNPROCESSABLE_ENTITY,
+        `Webhook Error: ${error?.message}`
       );
     }
   };
